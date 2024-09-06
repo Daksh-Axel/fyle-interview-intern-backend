123a6b0ccacc125a79f313eb14b3c4a0e31ad9f9
Daksh-Axel <dakshrajsadashiv@iitbhilai.com>
2024-09-06 11:20:20 +0530

    Corrected Assignment Submit class method, added calssmethod for Principal to get assignments

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -5,7 +5,7 @@ from core.libs import helpers, assertions
 from core.models.teachers import Teacher
 from core.models.students import Student
 from sqlalchemy.types import Enum as BaseEnum
-
+from sqlalchemy import or_
 
 class GradeEnum(str, enum.Enum):
     A = 'A'
@@ -65,8 +65,9 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
-
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
         assignment.teacher_id = teacher_id
+        assignment.state = AssignmentStateEnum.SUBMITTED
         db.session.flush()
 
         return assignment
@@ -91,3 +92,8 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_teacher(cls):
         return cls.query.all()
+    
+    @classmethod
+    # returning assignmnets with the state either SUBMITTED or GRADED
+    def get_assignments_by_principal(cls):
+         return cls.filter(or_(cls.state == "SUBMITTED", cls.state == "GRADED")).all()
